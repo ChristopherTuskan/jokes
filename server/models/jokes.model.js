9043ca800036c69efbf87343452553b892771a1d
Chris Tuskan <ctuskan@gmail.com>
2023-01-11 19:18:01 -0500

    first_commit

---
+++ server/models/jokes.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema({
+    setup: {
+        type: String
+    },
+    punchline: {
+        type: String
+    },
+    createdAt: {
+        type: Date
+    },
+    updatedAt: {
+        type: Date
+    }
+});
+
+const Joke = mongoose.model('Joke', JokeSchema);
+
+module.exports = Joke;
+
